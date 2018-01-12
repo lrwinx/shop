148cb5882c5e341e85f862d6e663e138bc98a277
dulei <tsl-dulei@tasly.com>
2018-01-12 13:42:51 +0800

    去掉了自定任务注解

--- user-service/user-service-core/src/main/java/com/tasly/UserServiceApplication.java
+++ user-service/user-service-core/src/main/java/com/tasly/UserServiceApplication.java
@@ -3,10 +3,8 @@ package com.tasly;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 
-@EnableScheduling
 @EnableEurekaClient
 @SpringBootApplication
 public class UserServiceApplication {
