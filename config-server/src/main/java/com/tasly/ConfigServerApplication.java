799895bb8611c827b6994c430c8238a80b99aa39
dulei <tsl-dulei@tasly.com>
2018-01-12 13:24:50 +0800

    将配置中心注册到服务发现中，客户端进行service命名引用

--- config-server/src/main/java/com/tasly/ConfigServerApplication.java
+++ config-server/src/main/java/com/tasly/ConfigServerApplication.java
@@ -3,7 +3,10 @@ package com.tasly;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+
+@EnableEurekaClient
 @EnableConfigServer
 @SpringBootApplication
 public class ConfigServerApplication {
