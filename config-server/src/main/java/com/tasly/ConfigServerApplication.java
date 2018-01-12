4fa24ba5f48d8ea45d1c66d481b37a071ee787ec
dulei <tsl-dulei@tasly.com>
2018-01-12 13:37:58 +0800

    使用@EnableDiscoveryClient注解替代@EnableEurekaClient

--- config-server/src/main/java/com/tasly/ConfigServerApplication.java
+++ config-server/src/main/java/com/tasly/ConfigServerApplication.java
@@ -2,11 +2,10 @@ package com.tasly;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-
-@EnableEurekaClient
+@EnableDiscoveryClient
 @EnableConfigServer
 @SpringBootApplication
 public class ConfigServerApplication {
